bddc00a7ced796e141ec1be978c83a5a784f6927
Ramonie <ramonmartinsmr@gmail.com>
2024-03-13 21:22:46 -0300

    desafio #3

--- __tests__/exercicios/Lista_1/Questao_6.test.js
+++ __tests__/exercicios/Lista_1/Questao_6.test.js
@@ -1,1 +1,8 @@
-//teste aqui
+import verificacao from "../../../src/exercicios/Lista_1/Questao_6";
+let texto;
+beforeEach(()=> {
+    texto = "[()[)";
+});
+test ("verificacao", ()=>{
+    expect(verificacao(texto)).toBe(false);
+});
--- src/exercicios/Lista_1/Questao_6.js
+++ src/exercicios/Lista_1/Questao_6.js
@@ -1,1 +1,47 @@
-//resolver aqui
+class Pilha {
+    constructor(tam = 10) {
+        this.dados = [];
+        this.topo = 0;
+        this.tam = tam;
+    }
+
+    push(dado) {
+        if (!this.isFull()) {
+            this.dados[this.topo++] = dado;
+            return;
+        }
+        throw new Error("Stackoverflow");
+    }
+
+    pop() {
+        if (!this.isEmpty()) {
+            this.topo--;
+            return;
+        }
+        throw new Error("Stackunderflow");
+    }
+
+    isEmpty() {
+        return this.topo === 0;
+    }
+
+    isFull() {
+        return this.topo === this.tam;
+    }
+}
+
+export default function verificacao(texto) {
+    let p = new Pilha(texto.length);
+    let aux;
+    for (let i = 0; i < texto.length; i++) {
+        if (texto[i] === '(' || texto[i] === '[') {
+            p.push(texto[i]);
+        } else {
+            aux = p.pop();
+            if ((aux === '(' && texto[i] !== ')') || (aux === '[' && texto[i] !== ']')) {
+                return false;
+            }
+        }
+    }
+    return p.isEmpty();
+}
